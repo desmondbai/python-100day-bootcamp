f0daf6f0a9ed1492c0f35cd51449d8625f4978a7
Desmond Bai <desmond.bai29@gmail.com>
2025-02-05 01:40:50 -0800

    adding main script for generating dot painting by Hirst

--- day18-tutle_and_GUIs/main.py
+++ day18-tutle_and_GUIs/main.py
@@ -1,92 +1,96 @@
-from turtle import Turtle,Screen,colormode,clearscreen
-from random import randint,choice,random
-from utils import *
-
-
-#Initialize a turtle object with color and shape
-my_turtle = Turtle()
-my_turtle.shape("circle")
-my_turtle.color("Red")
+from turtle import Turtle, Screen, colormode, Screen,mode
+import colorgram
+import random
 
 
 
+SPEED = 0
+THICKNESS = 5
+N_MAIN = 50 #VERTIAL
+N_CROSS = 50 #HORIZONTAL
+RADIUS = 5
+colormode(255)
+mode("standard")
 
+if __name__ == "__main__":
+    
+    my_turtle = Turtle()
+    my_turtle.shape("circle")
 
-screen = Screen()
+    #Pick color palette from downloaded image
+    colors = colorgram.extract("img.jpeg",10)
+    colors = [tuple(color.rgb) for color in colors]
+    print(colors)
 
-
-
-
-# Challenge1: Drawing a square
-turtle_write_text(my_turtle,"Challenge1: Drawing a Square")
-for _ in range(4):
-    my_turtle.forward(100)
-    my_turtle.right(90)
-
-clearscreen()
-
-
-
-
-# Challenge2: Drawing a dashed line
-turtle_write_text(my_turtle,"Challenge2: Drawing a dashed line")
-for step in range(50):
-    if step % 2 == 0:
-        my_turtle.penup()
-    else:
-        my_turtle.pendown()
-
-    my_turtle.forward(10)
-
-clearscreen()
+    # Start screen
+    screen = Screen()
+    screen.screensize(800,600)
+    width,height = screen.screensize()
+    print(width,height)
 
 
 
+    # Initialize turtle position
+    #my_turtle.hideturtle()
+    my_turtle.shape("arrow")
+    my_turtle.penup()
+    my_turtle.setpos(-width/2,-height/2)
+    my_turtle.setheading(0)
 
 
-# Challenge3: Drawing Polygons
-turtle_write_text(my_turtle,"Challenge3: Drawing Polygons",(-100,50))
-
-for side in range(4,11):
-    draw_polygon(my_turtle,side)
-
-clearscreen()
-
-
-
+    
 
 
 
-# Challenge4: Random Walk
+    # Calculate step size for x and y axis
+    step_cross = width / N_CROSS
+    step_main = height / N_MAIN
 
-turtle_write_text(my_turtle,"Challenge4: Random Walk")
 
-turtle_random_walk(my_turtle,speed=5,thickness=5,step_size=50,n_steps=50)
+    print(screen.screensize())
+    
 
-clearscreen()
+    # Initialize properties with param set-ups
+    my_turtle.width(THICKNESS)
+    my_turtle.speed(SPEED)
+
+
+
+    
+
+
+
+    def draw_circle(turtle:Turtle,radius:int,color_palette:list):
+        
+        # Store original heading
+        original_heading = turtle.heading()
+        # Fix drawing direction to only clock-wise
+        turtle.setheading(0)
+        turtle.fillcolor(random.choice(color_palette))
+        turtle.begin_fill()
+        turtle.circle(radius)
+        turtle.end_fill()
+
+        # Reset to original heading after circle is drawn
+        turtle.setheading(original_heading)
+
+
+
+
+    for _ in range(N_MAIN):
+        draw_circle(my_turtle,RADIUS,colors)
+        for _ in range(N_CROSS - 1):
+            my_turtle.forward(step_cross)
+            draw_circle(my_turtle,RADIUS,colors)        
+
+        opposite_heading = my_turtle.heading() + 180
+        my_turtle.setheading(90)
+        my_turtle.forward(step_main)
+        my_turtle.setheading(opposite_heading)
+        print(my_turtle.heading())
 
         
-        
 
-
-
-
-# Challenge5: Drawing a Spirograph
-my_turtle.shape("arrow")
-my_turtle.width(1)
-
-turtle_write_text(my_turtle,"Challenge5: Draw a Spiralgraph",(0,200))
-
-        
-draw_spiralgraph(my_turtle)
-
-
-
-
-screen.exitonclick()
-
-
-
-
+    screen.exitonclick()
 
 
